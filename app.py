a8e963ac3fc6abaeb49ee460e30514f1e59c1a69
Jack Simms <129862066+SoftwareSimms@users.noreply.github.com>
2024-01-17 22:50:10 +0000

    Added delivery_date
    added in Order class, route to display orders and session that interacts with database, added HTML to display new field also.

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ class Order(Base):
     product_code = Column('product_code', String)
     product_quantity = Column('Product Quantity', Integer)
     order_date = Column('Order Date', DateTime)
+    delivery_date = Column('Delivery Date', DateTime)
     shipping_date = Column('Shipping Date', DateTime)
+    
 
 # define routes
 # route to display orders
@@ -85,6 +87,7 @@ def add_order():
     product_quantity = request.form.get('product_quantity')
     order_date = request.form.get('order_date')
     shipping_date = request.form.get('shipping_date')
+    delivery_date = request.form['delivery_date']
     
     # Create a session to interact with the database
     session = Session()
@@ -98,7 +101,8 @@ def add_order():
         product_code=product_code,
         product_quantity=product_quantity,
         order_date=order_date,
-        shipping_date=shipping_date
+        shipping_date=shipping_date,
+        delivery_date=delivery_date
     )
 
     # Add the new order to the session and commit to the database
