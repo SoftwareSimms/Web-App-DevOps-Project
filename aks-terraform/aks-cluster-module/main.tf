67161ca43853cb4d18ac48661cfb7326b3f00cd1
Jack Simms <129862066+SoftwareSimms@users.noreply.github.com>
2024-01-18 17:55:12 +0000

    added ouputs and main terraform
    Added main.tf and outputs.tf to the AKS cluster module. The main.tf file sets up the AKS cluster, including node pools and service principal. The outputs.tf file captures essential details like the cluster name, ID, and Kubernetes configuration for easy management.

---
+++ aks-terraform/aks-cluster-module/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  service_principal {
+    client_id     = var.service_principal_client_id
+    client_secret = var.service_principal_client_secret
+  }
+
+  tags = var.tags
+}
+
---
+++ aks-terraform/aks-cluster-module/outputs.tf
@@ -0,0 +1,14 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+  description = "The name of the provisioned AKS cluster"
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+  description = "The ID of the provisioned AKS cluster"
+}
+
+output "aks_kubeconfig" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  description = "The Kubernetes configuration file for managing the AKS cluster"
+}
